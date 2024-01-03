87eea4b5c7e0eb76808cee2cb99833cb17e16288
caizhen <zhenchai0000@gmail.com>
2024-01-03 16:00:47 +0800

    add: footer

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    const year = new Date().getFullYear();
+    return (
+        <footer className="w-full border-t border-[#33353F]">
+            <div className="mb-14 w-full container mx-auto p-4 flex justify-between items-center text-slate-400">
+                <span>Cai Zhen</span>
+                <p>Copyright &copy; {year} All rights reserved</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
