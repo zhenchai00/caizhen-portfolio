513b5cdbb3c99cc2be8ba7f8e42496fa7b4a5c83
caizhen <zhenchai0000@gmail.com>
2024-01-05 16:51:30 +0800

    add: darkmode features

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,8 @@ import { Source_Code_Pro } from "next/font/google";
 import "../styles/globals.css";
 import NavBar from "@/components/NavigationBar/NavBar";
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
+import Script from "next/script";
 
 const SourceCodePro = Source_Code_Pro({
     weight: ["400", "700"],
@@ -30,11 +32,28 @@ export default function RootLayout({
     return (
         <html lang="en">
             <AppRouterCacheProvider>
-                <body className={`${SourceCodePro.variable} font-sans min-h-screen mx-auto text-black dark:text-white`}>
+                <Script id="darkmodescipt">
+                    {`
+                        if ('darkMode' in localStorage) {
+                            if (localStorage.getItem('darkMode') === 'true') {
+                                document.querySelector('html').classList.add('dark');
+                            } else {
+                                document.querySelector('html').classList.remove('dark');
+                            }
+                        } else {
+                            if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                                document.querySelector('html').classList.add('dark');
+                            } else {
+                                document.querySelector('html').classList.remove('dark');
+                            }
+                        }
+                    `}
+                </Script>
+                <body
+                    className={`${SourceCodePro.variable} font-sans min-h-screen mx-auto text-black dark:text-white bg-[#eff1f1] dark:bg-black`}
+                >
                     <NavBar />
-                    <div className="px-6 md:px-12">
-                        {children}
-                    </div>
+                    <div className="px-6 md:px-12">{children}</div>
                     <Analytics />
                 </body>
             </AppRouterCacheProvider>
