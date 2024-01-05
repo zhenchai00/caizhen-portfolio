7ecdb1c20b2757c7969761fa72fa343e24dac9e2
caizhen <zhenchai0000@gmail.com>
2024-01-05 15:15:38 +0800

    add: project section

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import Main from "@/components/Home/Main";
 import AboutMe from "@/components/Home/AboutMe";
 import Footer from "@/components/Footer";
 import Contact from "@/components/Contact";
+import Projects from "@/components/Projects/Projects";
 export default function Home() {
     return (
         <div className="min-h-screen flex flex-col">
             <Main />
             <AboutMe />
+            <Projects />
             <Contact />
             <Footer />
         </div>
---
+++ components/Projects/Projects.tsx
@@ -0,0 +1,42 @@
+const getUserRepos = async () => {
+    let res = await fetch(
+        "https://api.github.com/users/zhenchai00/repos?sort=updated&direction=desc&per_page=10&type=all"
+    );
+    let repos = await Promise.all([res.json()]);
+    return repos[0];
+};
+
+import React from "react";
+import ProjectItem from "@/components/Projects/ProjectsItem";
+
+const Projects = async () => {
+    let repos = await getUserRepos();
+    repos = repos
+        .sort((a: any, b: any) => b.stargazers_count - a.stargazers_count)
+        .filter((repo: any) => !repo.fork)
+        .filter((repo: any) => !repo.archived);
+
+    let gridRows = Math.ceil(repos.length / 3);
+
+    return (
+        <div className="pt-16" id="projects">
+            <h1 className="text-4xl font-bold mb-8">
+                Projects
+            </h1>
+            <div className={`grid gap-4 grid-cols-1 md:grid-cols-3 grid-rows-1 md:grid-rows-${gridRows}`}>
+                {repos.map((repo: any) => (
+                    <ProjectItem
+                        key={repo.id}
+                        name={repo.name}
+                        description={repo.description}
+                        forks_count={repo.forks_count}
+                        stargazers_count={repo.stargazers_count}
+                        url={repo.html_url}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Projects;
