8e56dec1f19bda63d99ec5d07097eb2a989a4b0d
caizhen <zhenchai0000@gmail.com>
2024-01-10 23:43:38 +0800

    update: footer mobile view

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     const year = new Date().getFullYear();
     return (
         <footer className="w-full border-t border-[#33353F]">
-            <div className="mb-14 w-full container mx-auto p-4 flex flex-col md:flex-row justify-between items-center text-slate-800 dark:text-slate-400">
+            <div className="mb-14 w-full container mx-auto p-4 flex flex-col md:flex-row justify-between items-center text-slate-800 dark:text-slate-400 text-center">
                 <span>Cai Zhen</span>
                 <p>Copyright &copy; {year} All rights reserved</p>
             </div>
