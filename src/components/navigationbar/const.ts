dcd4397cca1516c7a7d70abff466d6abc143942e
caizhen <zhenchai0000@gmail.com>
2026-07-05 16:10:24 +0800

    add photography portfolio redirect

--- src/components/navigationbar/const.ts
+++ src/components/navigationbar/const.ts
@@ -2,4 +2,5 @@ export const NAVIGATION_ITEMS = [
     { title: "Home", href: "/" },
     { title: "About", href: "/about" },
     { title: "Projects", href: "/projects" },
+    { title: "Photography", href: "https://sites.google.com/view/caizhen-photography/" },
 ];
